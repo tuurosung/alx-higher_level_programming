6ed55f01f52b1f0238669856f10417c871efd482
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-06 20:40:25 +0000

    SOlve pycodestyle error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for x in range(0, 99) :
-    if x == 99 :
+for x in range(0, 100):
+    if x == 99:
         print("{}".format(x))
-    else :
+    else:
         print("{:02}".format(x), end=", ")
