df28b9a33a81372e1bce615e9f77ebcdd16efcec
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-06 21:18:47 +0000

    Fix pycodestyle errors

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def uppercase(str):
     for letter in str:
-        if ord(letter) >=97 and ord(letter) <=122:
-            letter = chr(ord(letter) -32)
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            letter = chr(ord(letter) - 32)
         print("{}".format(letter), end="")
     print("")
