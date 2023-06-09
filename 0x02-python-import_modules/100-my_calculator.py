a01f2da05a974996a39b9d44204fca34fe10bbf9
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-09 02:01:50 +0000

    Solve task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,18 +6,18 @@ if __name__ == "__main__":
 
     import sys
 
-    #check if the parameters passed are exactly 3
+    """check if the parameters passed are exactly 3"""
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    #pass operators to variable
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    """pass operators to variable"""
+    oprtr = {"+": add, "-": sub, "*": mul, "/": div}
 
-    if sys.argv[2] not in list(operators.keys()):
+    if sys.argv[2] not in list(oprtr.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, oprtr[sys.argv[2]](a, b)))
