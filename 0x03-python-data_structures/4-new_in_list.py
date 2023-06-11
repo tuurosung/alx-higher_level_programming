064fbe3ac6a41b5f16533da6c86b74c0e85ae216
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-11 23:02:39 +0000

    Fix errors with Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,11 +2,11 @@
 
 def new_in_list(my_list, idx, element):
     """Duplicate list"""
-    list = my_list
+    list = my_list.copy()
     """check if idx is valid in list"""
     if idx < 0 or idx > (len(my_list) - 1):
-        return list
+        return my_list.copy()
 
     else:
         my_list[idx] = element
-        return my_list
+        return list
