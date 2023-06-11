b99ed84b9ca1f760ce0225a54ee59a898379fc96
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-11 21:49:44 +0000

    Solve task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,7 @@
 
 """Define the function"""
 def print_list_integer(my_list=[]):
+
     """Loop through the list of integers"""
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
