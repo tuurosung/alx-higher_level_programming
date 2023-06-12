783daf9ad99c749ffcd2ed5c6f8dc27f8273439c
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-12 16:57:12 +0000

    Solve task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    temp = [val for val in my_string if val !='c' and val != 'C']
+    temp = [val for val in my_string if val != 'c' and val != 'C']
     return ("".join(temp))
