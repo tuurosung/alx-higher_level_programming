c6efec9f768fb0827b644753f37f9c9550563718
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-14 23:31:02 +0000

    Fix error with function

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def simple_delete(a_dictionary):
+def multipy_by_2(a_dictionary):
     dup = {}
     for key, value in a_dictionary.items():
         dup.update({key: (value * 2)})
