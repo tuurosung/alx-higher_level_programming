a9a0186500df1f6188ac8f20577eb796730e490f
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-14 23:33:30 +0000

    Fix spelling error with function

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def multipy_by_2(a_dictionary):
+def multiply_by_2(a_dictionary):
     dup = {}
     for key, value in a_dictionary.items():
         dup.update({key: (value * 2)})
