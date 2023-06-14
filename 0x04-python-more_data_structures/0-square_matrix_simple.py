10f99da4bf7b16936208264047bf46c9375caef5
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-14 22:39:59 +0000

    Fix matrix error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x, line)) for line in matrix])
+    return ([list(map(lambda x: x * x, line)) for line in matrix])
