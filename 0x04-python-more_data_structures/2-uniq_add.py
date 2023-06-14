cdcab4dd597752940784d3c0f08d8f39053361ce
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-14 22:42:20 +0000

    Fix usr/bin error

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,9 @@
-#!/usr/python3
+#!/usr/bin/python3
 
 def uniq_add(my_list=[]):
 
     """initialize variable sum"""
     sum = 0
-
     for val in set(my_list):
         sum += val
     return (sum)
