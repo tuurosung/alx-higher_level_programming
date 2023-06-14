da801a942be49d569a7ca761e8f80c57494fc702
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-14 22:35:52 +0000

    Solve Task 0 to 2

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+
+    """create new copy of my list"""
+    dup = my_list
+
+    for i in range(len(dup)):
+        if dup[i] == search:
+            dup[i] = replace
+
+    return (dup)
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/python3
+
+def uniq_add(my_list=[]):
+
+    """initialize variable sum"""
+    sum = 0
+
+    for val in set(my_list):
+        sum += val
+    return (sum)
