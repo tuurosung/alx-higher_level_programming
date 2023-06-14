d0777388e763ffa6a8ff936b4f448928bfda77d0
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-14 22:41:12 +0000

    Fix error on line 6

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
 
     """create new copy of my list"""
-    dup = my_list
+    dup = my_list[:]
 
     for i in range(len(dup)):
         if dup[i] == search:
