627cfe9e352a614cd3f99a90498cc58843585eef
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-15 02:41:51 +0000

    Fix errors on task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    dup = my_list.copy()
-    return (list(map((lambda x: x * number), dup)))
+    return (list(map((lambda x: x * number), my_list)))
