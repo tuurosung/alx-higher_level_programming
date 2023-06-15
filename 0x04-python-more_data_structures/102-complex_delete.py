794c02205993c7752f5110010631c388a4eeea44
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-15 03:16:17 +0000

    Solve Complex Delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    while value in a_dictionary.values():
+        for x, y in a_dictionary.items():
+            if y == value:
+                del a_dictionary[x]
+                break
+
+    return (a_dictionary)
