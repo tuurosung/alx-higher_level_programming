241b278ebb0617a2b1e3a7a8c2d6e06d67f6e67a
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-27 01:44:54 +0000

    Reset result to zero in loop

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,13 +13,11 @@ def list_division(my_list_1, my_list_2, list_length):
                 result = my_list_1[i] / my_list_2[i]
             except TypeError:
                 print("wront type")
-                result = 0
             except ZeroDivisionError:
                 print("division by 0")
-                result = 0
             except IndexError:
                 print("out of range")
-                result = 0
             finally:
                 new_list.append(result)
+                result = 0
         return (new_list)
