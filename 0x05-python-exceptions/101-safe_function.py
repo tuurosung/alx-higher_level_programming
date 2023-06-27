e07d265767dc5c4f65d0884a5dc78eb40a8fe3d7
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-27 02:32:44 +0000

    Fix pcodestyle error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_function(fct, *args):
-    result = None
+    res = None
 
     try:
-        resutl = fct(*args)
+        res = fct(*args)
 
     except Exception as err:
         sys.stderr.write("Exception: " + err.__str__() + "\n")
         return None
 
     else:
-        return result
+        return res
