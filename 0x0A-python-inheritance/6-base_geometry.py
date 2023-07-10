144ce2ef63181f75914ee6949924033a8a68f7c9
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-07-10 22:40:21 +0000

    Solve 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define the base geo class"""
+
+
+class BaseGeometry:
+    """Base class for"""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
