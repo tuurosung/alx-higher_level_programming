6b373ae794ed1d88584d1c5b9bf7cac7aa314fd1
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-07-10 21:43:41 +0000

    1 Corrections

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,9 @@
 
 class MyList(list):
     """Define a public instance method """
+    def __init__(self):
+        """initialize"""
+        super().__init__()
 
     def print_sorted(self):
         """Print a sorted list """
