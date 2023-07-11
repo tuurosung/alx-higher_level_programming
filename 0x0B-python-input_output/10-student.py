00b617cc8414f8dfd830e14d4f4476a736cad402
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-07-11 23:11:58 +0000

    Solve 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,6 +15,6 @@ class Student:
     def to_json(self, attrs=None):
         """Get a dictionary representation"""
         if (type(attrs) == list and
-                    all(type(ele) == str for ele in attrs)):
+                all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
