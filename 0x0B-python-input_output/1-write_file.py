5e3a6185c008896fb8f8fec9168183304eb7332b
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-07-11 21:40:09 +0000

    Solve 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A function that writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
