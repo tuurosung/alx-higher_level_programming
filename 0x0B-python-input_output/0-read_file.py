d89f04ff3da443206ddd47912045bb85db34abe4
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-07-11 23:23:12 +0000

    Fix error with 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A function that reads a file and prints it to stdout"""
+
+
 def read_file(filename=""):
     """Reads a file and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
