9952b02b53bc07aa7dde9e7a349e2ab9a313f24d
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-07-11 23:07:39 +0000

    Solve 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,6 +8,10 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """Initialize a Student"""
 
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
     def to_json(self):
         """Get a dictionary representation"""
         return self.__dict__
