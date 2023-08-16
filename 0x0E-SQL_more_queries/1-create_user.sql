9884da04ac5e753836ee173c548afdc869f89ad4
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-08-16 23:05:46 +0000

    Solve 1 and 0

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,9 @@
--- Create a new mysql user and grand all privileges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+-- Root User
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
