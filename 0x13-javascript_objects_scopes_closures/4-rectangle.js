9141891de56e9ded852833bdcec4270815329488
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-09-12 22:20:24 +0000

    Solve 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h] }
   }
 
   print () {
@@ -9,10 +9,10 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    [this.width, this.height] = [this.height, this.width]
   }
 
   double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+    [this.width, this.height] = [this.width * 2, this.height * 2]
   }
-};
+}
