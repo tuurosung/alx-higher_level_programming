b1bcbdaed3ae1ed1b2e06b7294881f7c0e481ca7
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-09-17 22:39:54 +0000

    Solve 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-#Lists all the states from lists all states from
+# Lists all the states from lists all states from
 # the database hbtn_0e_0_usa:
 
-#!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
