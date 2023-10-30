ba5cb53f892f3a9f4e65fddaae8cb4fbf43e14d2
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-10-30 02:00:54 +0000

    Solve 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const request = require('request')
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2]
 request(url, function (error, response, body) {
-    if (!error) {
-        const characters = JSON.parse(body).characters;
-        console.log(characters)
-        characters.forEach((character) => {
-            request(character, function (error, response, body) {
-                if (!error) {
-                    console.log(JSON.parse(body).name);
-                }
-            });
-        });
-    }
-});
+  if (!error) {
+    const characters = JSON.parse(body).characters
+    console.log(characters)
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name)
+        }
+      })
+    })
+  }
+})
